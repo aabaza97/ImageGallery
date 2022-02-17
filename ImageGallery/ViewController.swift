8a1bcf7cc3b115404d4d83736d32f487f3147a30
Ahmed Abaza <aabaza32@gmail.com>
2022-02-17 14:35:51 +0200

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Ahmed Abaza on 17/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
